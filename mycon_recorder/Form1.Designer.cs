31ceb2231ab6f2ce7019c30c9c0f108df122adb9
rsna6ce <86136223+rsna6ce@users.noreply.github.com>
2024-08-14 16:11:19 +0900

    added first files.

---
+++ mycon_recorder/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace mycon_recorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonRec = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.checkBoxWaiging = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.listBoxMessageLog = new System.Windows.Forms.ListBox();
+            this.labelLatestMessage = new System.Windows.Forms.Label();
+            this.timerPlay = new System.Windows.Forms.Timer(this.components);
+            this.textBoxIPAddr = new System.Windows.Forms.TextBox();
+            this.labelPlayTime = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(263, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openOToolStripMenuItem,
+            this.saveAsSToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.fileToolStripMenuItem.Text = "File(&F)";
+            // 
+            // openOToolStripMenuItem
+            // 
+            this.openOToolStripMenuItem.Name = "openOToolStripMenuItem";
+            this.openOToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openOToolStripMenuItem.Text = "Open(&O)";
+            this.openOToolStripMenuItem.Click += new System.EventHandler(this.openOToolStripMenuItem_Click);
+            // 
+            // saveAsSToolStripMenuItem
+            // 
+            this.saveAsSToolStripMenuItem.Name = "saveAsSToolStripMenuItem";
+            this.saveAsSToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsSToolStripMenuItem.Text = "Save as (&S)";
+            this.saveAsSToolStripMenuItem.Click += new System.EventHandler(this.saveAsSToolStripMenuItem_Click);
+            // 
+            // buttonRec
+            // 
+            this.buttonRec.Location = new System.Drawing.Point(4, 28);
+            this.buttonRec.Name = "buttonRec";
+            this.buttonRec.Size = new System.Drawing.Size(84, 36);
+            this.buttonRec.TabIndex = 1;
+            this.buttonRec.Text = "REC";
+            this.buttonRec.UseVisualStyleBackColor = true;
+            this.buttonRec.Click += new System.EventHandler(this.buttonRec_Click);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(4, 68);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(84, 36);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.Text = "PLAY";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // checkBoxWaiging
+            // 
+            this.checkBoxWaiging.AutoSize = true;
+            this.checkBoxWaiging.Checked = true;
+            this.checkBoxWaiging.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxWaiging.Location = new System.Drawing.Point(96, 40);
+            this.checkBoxWaiging.Name = "checkBoxWaiging";
+            this.checkBoxWaiging.Size = new System.Drawing.Size(120, 16);
+            this.checkBoxWaiging.TabIndex = 2;
+            this.checkBoxWaiging.Text = "Wait first message";
+            this.checkBoxWaiging.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "start delay[s]";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(176, 68);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(48, 19);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // listBoxMessageLog
+            // 
+            this.listBoxMessageLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxMessageLog.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.listBoxMessageLog.FormattingEnabled = true;
+            this.listBoxMessageLog.ItemHeight = 16;
+            this.listBoxMessageLog.Items.AddRange(new object[] {
+            "999.999 UDLRABCDLlRrET12H"});
+            this.listBoxMessageLog.Location = new System.Drawing.Point(4, 140);
+            this.listBoxMessageLog.Name = "listBoxMessageLog";
+            this.listBoxMessageLog.Size = new System.Drawing.Size(252, 164);
+            this.listBoxMessageLog.TabIndex = 3;
+            // 
+            // labelLatestMessage
+            // 
+            this.labelLatestMessage.BackColor = System.Drawing.SystemColors.Window;
+            this.labelLatestMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelLatestMessage.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelLatestMessage.Location = new System.Drawing.Point(4, 120);
+            this.labelLatestMessage.Name = "labelLatestMessage";
+            this.labelLatestMessage.Size = new System.Drawing.Size(252, 20);
+            this.labelLatestMessage.TabIndex = 4;
+            this.labelLatestMessage.Text = "000.000 UDLRABCDLlRrET12H";
+            // 
+            // timerPlay
+            // 
+            this.timerPlay.Enabled = true;
+            this.timerPlay.Interval = 1;
+            this.timerPlay.Tick += new System.EventHandler(this.timerPlay_Tick);
+            // 
+            // textBoxIPAddr
+            // 
+            this.textBoxIPAddr.Location = new System.Drawing.Point(96, 88);
+            this.textBoxIPAddr.Name = "textBoxIPAddr";
+            this.textBoxIPAddr.Size = new System.Drawing.Size(128, 19);
+            this.textBoxIPAddr.TabIndex = 6;
+            this.textBoxIPAddr.Text = "192.168.0.255";
+            // 
+            // labelPlayTime
+            // 
+            this.labelPlayTime.AutoSize = true;
+            this.labelPlayTime.Location = new System.Drawing.Point(28, 103);
+            this.labelPlayTime.Name = "labelPlayTime";
+            this.labelPlayTime.Size = new System.Drawing.Size(43, 12);
+            this.labelPlayTime.TabIndex = 7;
+            this.labelPlayTime.Text = "999.999";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 309);
+            this.Controls.Add(this.labelPlayTime);
+            this.Controls.Add(this.textBoxIPAddr);
+            this.Controls.Add(this.labelLatestMessage);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBoxWaiging);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonRec);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.listBoxMessageLog);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "mycon_recorder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsSToolStripMenuItem;
+        private System.Windows.Forms.Button buttonRec;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.CheckBox checkBoxWaiging;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxMessageLog;
+        private System.Windows.Forms.Label labelLatestMessage;
+        private System.Windows.Forms.Timer timerPlay;
+        private System.Windows.Forms.TextBox textBoxIPAddr;
+        private System.Windows.Forms.Label labelPlayTime;
+    }
+}
+
---
+++ mycon_recorder/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.IO;
+
+namespace mycon_recorder
+{
+    public partial class Form1 : Form
+    {
+        private UdpClient _udpClient = null;
+        private const int _port = 59630;
+        private const int _port_offset_for_debug = 1;
+        private System.Diagnostics.Stopwatch _sw;
+        private bool _recording = false;
+        private bool _recording_waiting = false;
+        private long _recording_started = 0;
+        private bool _playing = false;
+        private long _playing_started = 0;
+        private int _playing_index = 0;
+        private Color _button_color_default;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBoxMessageLog.Items.Clear();
+            labelLatestMessage.Text = "";
+            _sw = System.Diagnostics.Stopwatch.StartNew();
+
+            bool success = false;
+            int port_offset = 0;
+            do
+            {
+                try
+                {
+                    IPEndPoint localEP = new IPEndPoint(IPAddress.Any, _port + port_offset);
+                    _udpClient = new UdpClient(localEP);
+                    _udpClient.BeginReceive(ReceiveCallback, _udpClient);
+                    success = true;
+                }
+                catch
+                {
+                    //retry another port
+                    port_offset++;
+                }
+            } while (!success);
+
+            _button_color_default = buttonRec.BackColor;
+            labelPlayTime.Text = "";
+        }
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            UdpClient udp = (UdpClient)ar.AsyncState;
+
+            System.Net.IPEndPoint remoteEP = null;
+            byte[] rcvBytes;
+            try
+            {
+                rcvBytes = udp.EndReceive(ar, ref remoteEP);
+            }
+            catch (System.Net.Sockets.SocketException ex)
+            {
+                Console.WriteLine("ERROR: udp receive({0}/{1})", ex.Message, ex.ErrorCode);
+                return;
+            }
+            catch (ObjectDisposedException ex)
+            {
+                Console.WriteLine("ERROR: udp socket closed.({0})", ex.Message);
+                return;
+            }
+
+            string rcvMsg = System.Text.Encoding.UTF8.GetString(rcvBytes);
+            string recvTimeBase = String.Format("{0, 8}", (_sw.ElapsedMilliseconds / 1000.0).ToString("0.000"));
+            if (_recording && _recording_waiting)
+            {
+                _recording_started = _sw.ElapsedMilliseconds;
+                _recording_waiting = false;
+            }
+            this.Invoke(new Action<string>(this.SetMessage), recvTimeBase + " " + rcvMsg);
+
+            if (rcvMsg.EndsWith("H"))
+            {
+                string sendMsg = "H";
+                byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(sendMsg);
+                udp.Send(sendBytes, sendBytes.Length, remoteEP.Address.ToString(), remoteEP.Port);
+            }
+            else if (_recording)
+            {
+                string recvTimeElapsed = String.Format("{0, 8}", ((_sw.ElapsedMilliseconds- _recording_started) / 1000.0).ToString("0.000"));
+                this.Invoke(new Action<string>(this.AppendMessageLog), recvTimeElapsed + " " + rcvMsg);
+            }
+            udp.BeginReceive(ReceiveCallback, udp);
+        }
+
+        private void SetMessage(string msg)
+        {
+            this.labelLatestMessage.Text = msg;
+        }
+        private void AppendMessageLog(string msg)
+        {
+            listBoxMessageLog.Items.Add(msg);
+            listBoxMessageLog.SelectedIndex = listBoxMessageLog.Items.Count - 1;
+        }
+
+        
+        private void buttonRec_Click(object sender, EventArgs e)
+        {
+            if (_recording)
+            {
+                // stop rec
+                _recording = false;
+                buttonRec.BackColor = _button_color_default;
+                fileToolStripMenuItem.Enabled = true;
+                buttonPlay.Enabled = true;
+            }
+            else
+            {
+                // start rec
+                listBoxMessageLog.Items.Clear();
+                _recording_waiting = checkBoxWaiging.Checked;
+                if (!_recording_waiting)
+                {
+                    _recording_started = _sw.ElapsedMilliseconds;
+                }
+                _recording = true;
+                buttonRec.BackColor = Color.Red;
+                buttonPlay.Enabled = false;
+                fileToolStripMenuItem.Enabled = false;
+            }
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            SetPlay(!_playing);
+        }
+        private void SetPlay(bool play_mode)
+        {
+            if (!play_mode)
+            {
+                //stop play
+                buttonPlay.BackColor = _button_color_default;
+                buttonRec.Enabled = true;
+                textBoxIPAddr.ReadOnly = false;
+                listBoxMessageLog.Enabled = true;
+                fileToolStripMenuItem.Enabled = true;
+                _playing = false;
+            }
+            else
+            {
+                // start play
+                buttonPlay.BackColor = Color.Green;
+                buttonRec.Enabled = false;
+                _playing_started = _sw.ElapsedMilliseconds + (long)(numericUpDown1.Value * 1000);
+                textBoxIPAddr.ReadOnly = true;
+                listBoxMessageLog.Enabled = false;
+                fileToolStripMenuItem.Enabled = false;
+                _playing_index = 0;
+                _playing = true;
+            }
+
+        }
+
+        private void timerPlay_Tick(object sender, EventArgs e)
+        {
+            if (!_playing)
+            {
+                return;
+            }
+            if (listBoxMessageLog.Items.Count <= _playing_index)
+            {
+                SetPlay(false);
+                return;
+            }
+            if (listBoxMessageLog.SelectedIndex != _playing_index)
+            {
+                listBoxMessageLog.SelectedIndex = _playing_index;
+            }
+            long time_curr = _sw.ElapsedMilliseconds;
+            long time_elap_ms = time_curr - _playing_started;
+            labelPlayTime.Text = ((double)time_elap_ms / 1000.0).ToString("0.000");
+            double time_record_sec = double.Parse(listBoxMessageLog.Items[_playing_index].ToString().Substring(0,8));
+            long time_record_ms = (long)(time_record_sec * 1000);
+            if (time_record_ms <= time_elap_ms)
+            {
+                System.Net.IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse(textBoxIPAddr.Text), _port + _port_offset_for_debug);
+                string sendMsg = listBoxMessageLog.Items[_playing_index].ToString().Substring(9);
+                byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(sendMsg);
+                _udpClient.Send(sendBytes, sendBytes.Length, remoteEP.Address.ToString(), remoteEP.Port);
+                _playing_index++;
+            }
+        }
+
+        private void saveAsSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listBoxMessageLog.Items.Count == 0)
+            {
+                return;
+            }
+            DateTime now = DateTime.Now;
+            var time_now = now.ToString("yyyyMMdd_HHmmss");
+            string exe_dir = Path.GetDirectoryName(Application.ExecutablePath) + @"\";
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = time_now + ".txt";
+            sfd.InitialDirectory = exe_dir;
+            sfd.Filter = "TEXTファイル(*.txt)|*.txt|すべてのファイル(*.*)|*.*";
+            sfd.Title = "Select file to save.";
+            sfd.RestoreDirectory = true;
+            sfd.OverwritePrompt = true;
+            sfd.CheckPathExists = true;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(sfd.FileName, false))
+                {
+                    foreach(var item in listBoxMessageLog.Items) {
+                        writer.WriteLine(item);
+                    }
+                }
+            }
+        }
+
+        private void openOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string exe_dir = Path.GetDirectoryName(Application.ExecutablePath) + @"\";
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.FileName = "";
+            ofd.InitialDirectory = exe_dir;
+            ofd.Filter = "TEXTファイル(*.txt)|*.txt|すべてのファイル(*.*)|*.*";
+            ofd.Title = "Select file to open.";
+            ofd.RestoreDirectory = true;
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                listBoxMessageLog.Items.Clear();
+                using (StreamReader reader = new StreamReader(ofd.FileName, false))
+                {
+                    while (reader.Peek() >= 0)
+                    {
+                        listBoxMessageLog.Items.Add(reader.ReadLine());
+                    }
+                }
+            }
+        }
+    }
+}
